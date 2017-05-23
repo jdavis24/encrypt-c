172d82ea074a344306f95d26ddd8a3dd40004bef
Davis <JED141@pitt.edu>
2017-05-23 15:51:27 -0400

    Add project files.

---
+++ encrypt-c/Encrypt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace encrypt_c
+{
+    public partial class Encrypt
+    {
+
+        Outlook.Inspectors inspectors;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            inspectors = this.Application.Inspectors;
+        }
+        
+        #region VSTO generated code
+
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+        }
+        
+        #endregion
+    }
+}
---
+++ encrypt-c/EncryptButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Outlook;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace encrypt_c
+{
+    public partial class EncryptButton
+    {
+        private void EncryptButton_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void EncryptBtn_Click(object sender, RibbonControlEventArgs e)
+        {
+            Outlook.Application app = new Outlook.Application();
+            Outlook.MailItem mailItem = app.CreateItem(Outlook.OlItemType.olMailItem);
+            mailItem.Subject = "Encrypt: ";
+            mailItem.Display(false);
+            
+        }
+    }
+}
